a0d365d0a2446441b1a21c0197c68bf4af921480
rubenoroz <rubenoroz@gmail.com>
2025-11-11 10:31:21 -0600

    Fix: Redeploy6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Manrope, Playfair_Display, Inter } from "next/font/google";
 import "./globals.css";
 import "swiper/css";
@@ -8,16 +7,6 @@ import "swiper/css/pagination";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import SessionProvider from "@/components/SessionProvider";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const manrope = Manrope({
   variable: "--font-manrope",
@@ -51,7 +40,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${manrope.variable} ${playfairDisplay.variable} ${inter.variable} font-inter antialiased`}
+        className={`${manrope.variable} ${playfairDisplay.variable} ${inter.variable} font-inter antialiased`}
       >
         <SessionProvider>
           <div className="flex flex-col min-h-screen">
