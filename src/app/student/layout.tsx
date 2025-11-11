e88080dddfefdc1b9388c732eca0338fdaec23db
rubenoroz <rubenoroz@gmail.com>
2025-11-11 11:15:26 -0600

    fix:redeploy13

--- src/app/student/layout.tsx
+++ src/app/student/layout.tsx
@@ -1,6 +1,7 @@
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import { redirect } from 'next/navigation';
+import Link from 'next/link';
 
 export default async function StudentLayout({
   children,
